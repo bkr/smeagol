d467473a8a715592a70d7e4cdf10a7fddb373efb
Cameron Booth <cameronbooth@unknowne4ce8f099b0e.gateway.2wire.net>
2012-06-03 19:41:55 -0700

    Clean up the oh-my-zsh install a bit, with a guard against running, and creation of a folder it's looking for

--- cookbooks/oh-my-zsh/recipes/default.rb
+++ cookbooks/oh-my-zsh/recipes/default.rb
@@ -3,10 +3,15 @@
 # Recipe:: default
 #
 
+directory "#{ENV['HOME']}/.mainstay" do
+  action :create
+end
+
 script "oh-my-zsh install from github" do
   interpreter "bash"
   code <<-EOS
-    source ~/.mainstay.profile
-    curl https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -o - | sh
+    source ~/.mainstay/profile
+    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
   EOS
+  not_if  "test -e ~/.oh-my-zsh"
 end
