1798b074ee0d30e18db085f6c44b9325d1ca0a5a
Cameron Booth <cameron.booth@bookrenter.com>
2012-06-07 19:32:47 -0700

    Nginx/passenger rework, seems generally working now

--- cookbooks/bookrenter/recipes/nginx_and_passenger.rb
+++ cookbooks/bookrenter/recipes/nginx_and_passenger.rb
@@ -7,38 +7,59 @@
 # require root + '/providers/homebrew'
 # require 'etc'
 
-nginx_conf_path = "#{ENV['HOME']}/Developer/etc/nginx"
-nginx_logs_path = "#{ENV['HOME']}/Developer/var/log/nginx"
+nginx_conf_path = "/usr/local/nginx/conf"
+nginx_logs_path = "/usr/local/nginx/logs"
 
 script "create and set the rails3 gemset as the default" do
   interpreter "bash"
   code <<-EOS
-    rvm --create ree@rails3
-    rvm use ree@rails3 --default
+    rvm gemset create rails3
+    rvm use ruby-1.9.3-p125@rails3 --default
   EOS
 end
 
 script "installing passenger" do
   interpreter "bash"
   code <<-EOS
-    rvm ree@rails3 gem install passenger -v=3.0.9
+    rvm list
+    echo `rvm list`
+    rvm ruby-1.9.3-p125 ; echo `ruby --version`
+    rvm ruby-1.9.3-p125@rails3 gem install passenger -v=3.0.9
   EOS
-  not_if "rvm ree@rails3 gem list passenger | grep -q \"3.0.9\""
+  not_if "gem list passenger | grep -q \"3.0.9\""
 end
 
-script "installing nginx" do
- interpreter "bash"
- code <<-EOS
-   echo "I'm INSTALLING nginx"
-   brew install nginx --with-passenger
- EOS
- not_if do
-   current_nginx_version = `#{ENV['HOME']}/Developer/sbin/nginx -V 2>&1`
-   current_nginx_version =~ /1\.0\.11/ ? true : false
- end
- 
+script "installing nginx via passsenger" do
+  interpreter "bash"
+  code <<-EOS
+    echo "nginx install start"
+    passenger-install-nginx-module --auto-download --auto --prefix=/usr/local/nginx
+    echo "nginx install end"
+  EOS
+   not_if do
+     current_nginx_version = `/usr/local/nginx/sbin/nginx -V 2>&1`
+     current_nginx_version =~ /1\.0\.15/ ? true : false
+   end
 end
 
+script "Putting nginx bin folder into path" do
+  interpreter "bash"
+  code "echo '\nPATH=\"$PATH:/usr/local/nginx/sbin\"; export PATH\n' >> ~/.mainstay/profile"
+  not_if  "grep -q '/usr/local/nginx/sbin' ~/.mainstay/profile"
+end
+
+# script "installing nginx" do
+#  interpreter "bash"
+#  code <<-EOS
+#    echo "I'm INSTALLING nginx"
+#  EOS
+#  not_if do
+#    current_nginx_version = `#{ENV['HOME']}/Developer/sbin/nginx -V 2>&1`
+#    current_nginx_version =~ /1\.0\.11/ ? true : false
+#  end
+#  
+# end
+# 
 %w{local cart.local ops.local bws.local store.local stores.local}.each do |url|
   execute "Adding #{url}.bookrenter.com to /etc/hosts" do
     command "echo '127.0.0.1 #{url}.bookrenter.com' | sudo tee -a /etc/hosts > /dev/null"
@@ -46,30 +67,30 @@ end
   end  
 end
 
-# script "adding local.bookrenter apps to /etc/hosts" do
-#   interpreter "bash"
-#   code <<-EOS
-#     echo "127.0.0.1 local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
-#     echo "127.0.0.1 ops.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
-#     echo "127.0.0.1 cart.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
-#     echo "127.0.0.1 store.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
-#     echo "127.0.0.1 stores.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
-#     echo "127.0.0.1 bws.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
-#   EOS
-#   not_if "cat /etc/hosts | grep local.bookrenter.com"
-# end  
+script "adding local.bookrenter apps to /etc/hosts" do
+  interpreter "bash"
+  code <<-EOS
+    echo "127.0.0.1 local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
+    echo "127.0.0.1 ops.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
+    echo "127.0.0.1 cart.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
+    echo "127.0.0.1 store.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
+    echo "127.0.0.1 stores.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
+    echo "127.0.0.1 bws.local.bookrenter.com" | sudo tee -a /etc/hosts > /dev/null
+  EOS
+  not_if "cat /etc/hosts | grep local.bookrenter.com"
+end  
 
 # system("mkdir -p #{ENV['HOME']}/Library/LaunchAgents")
 # system("launchctl unload -w -F #{destination_plist} >/dev/null 2>&1")
 # system("cp -f #{plist_fullpath_for(name)} #{destination_plist} >/dev/null 2>&1")
 # system("launchctl load -w -F #{destination_plist} >/dev/null 2>&1")
-
+# 
 # 
 # You can start nginx automatically on login running as your user with:
 #   mkdir -p ~/Library/LaunchAgents
 #   cp /Users/cameron/Developer/Cellar/nginx/1.0.6/org.nginx.nginx.plist ~/Library/LaunchAgents/
 #   launchctl load -w ~/Library/LaunchAgents/org.nginx.nginx.plist
-
+# 
 directory "#{nginx_conf_path}/certs" do
   action :create
 end
